2cc60201e441545e67a137a76a030ffb13e05391
dbolotov <dbolotov@live.com>
2012-08-06 16:14:36 -0700

    added sigmoid and sigmoidGradient funcs

--- nn_template.py
+++ nn_template.py
@@ -2,3 +2,37 @@
 #Filename: nn_template.py
 
 #Description
+
+
+
+#Code is based on ml-class.org, Ex.4.
+
+import sys, numpy as np
+from numpy import mat, c_, r_, array, e, reshape
+from scipy import optimize as op
+import itertools
+
+
+# Define functions
+
+def sigmoid(z):
+	g = 1./(1 + e**(-z))
+	return g
+
+def sigmoidGradient(z):
+	#must convert to array first
+	if type(z) != 'numpy.ndarray':
+		z = array([z])
+	f = 1./(1 + e**(-z))
+	return f*(np.ones(f.shape[0]) - f)
+
+#procedure
+
+
+
+
+
+
+
+
+
