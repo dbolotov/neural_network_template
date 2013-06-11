a2e61ae2db7d23acf5aea3bd1c9f997bb5907f17
dbolotov <dbolotov@live.com>
2013-06-11 15:58:16 -0700

    added print out of confusion matrix

--- nn_template.py
+++ nn_template.py
@@ -14,7 +14,9 @@ from nn_helper_functions import *
 start_time = time.time()
 
 
-X_full,y_full = data_preprocess('datasets/wdbc_data.csv')
+# X_full,y_full = data_preprocess('datasets/wdbc_data.csv')
+X_full,y_full = data_preprocess('dataset_07.csv')
+
 X,y,X_test,y_test = split_data(X_full, y_full, train_frac = 0.70)
 
 
@@ -23,8 +25,16 @@ Theta1, Theta2 = nn_train(X,y, lam=1, hidden_layer_size = 10)
 p_train = pred_accuracy(Theta1, Theta2, X, y)
 p_test = pred_accuracy(Theta1, Theta2, X_test, y_test)
 
+y_test_pred = predict(Theta1, Theta2, X_test)
+cm = confusion_matrix(y_test, y_test_pred)
+
 print '\nAccuracy on training set: %g' % p_train
-print 'Accuracy on test set: %g' % p_test
+print '\nAccuracy on test set: %g' % p_test
+
+print '\nConfusion matrix:\n',cm
+
+# print '\nTrue vs Predicted\n', c_[y_test, y_test_pred]
+
 
 print "Total run time:", time.time() - start_time, "seconds"
 
