e6b25429785223182a459490bacbe8132e9508a7
dbolotov <dbolotov@live.com>
2013-06-11 15:07:32 -0700

    add code to split into training and test sets

--- nn_template.py
+++ nn_template.py
@@ -14,9 +14,22 @@ from nn_helper_functions import *
 start_time = time.time()
 
 
-X,y,X_test,y_test = data_preprocess('datasets/fisher_iris.csv')
+X_full,y_full = data_preprocess('datasets/fisher_iris.csv')
 
-Theta1, Theta2 = nn_train(X,y)
+
+# Split input file into training and test files
+train_frac = 0.70
+test_rows = int(round(X_full.shape[0] * (1 - train_frac))) #num of rows in test set
+X_test = X_full[:test_rows, :] #test set
+y_test = y_full[:test_rows] #test set
+
+X = X_full[test_rows:,:] #training set
+y = y_full[test_rows:] #training set
+
+print type(X)
+print X.shape
+
+Theta1, Theta2 = nn_train(X,y, lam=1, hidden_layer_size = 10)
 
 p_train = pred_accuracy(Theta1, Theta2, X, y)
 
@@ -25,7 +38,7 @@ p_test = pred_accuracy(Theta1, Theta2, X_test, y_test)
 print '\nAccuracy on training set: %g' % p_train
 print 'Accuracy on test set: %g' % p_test
 
-print "Program run time:", time.time() - start_time, "seconds"
+print "Total run time:", time.time() - start_time, "seconds"
 
 
 
