ecf64441e68131e4524dbb3295363a01da2d8d45
dbolotov <dbolotov@live.com>
2013-06-24 10:22:48 -0700

    cleaned up comments

--- nn_template.py
+++ nn_template.py
@@ -13,27 +13,24 @@ from nn_helper_functions import *
 
 start_time = time.time()
 
-
+# load, shuffle and normlaize data
 X_full,y_full = data_preprocess('datasets/wdbc_data.csv')
-# X_full,y_full = data_preprocess('dataset_07.csv')
 
+# split data into training and test sets
 X,y,X_test,y_test = split_data(X_full, y_full, train_frac = 0.70)
 
-
+# learn network parameters
 Theta1, Theta2 = nn_train(X,y, lam=1, hidden_layer_size = 10)
 
+# evaluate performance
 p_train = pred_accuracy(Theta1, Theta2, X, y)
 p_test = pred_accuracy(Theta1, Theta2, X_test, y_test)
-
 y_test_pred = predict(Theta1, Theta2, X_test)
 cm = confusion_matrix(y_test, y_test_pred)
 
 print '\nAccuracy on training set: %g' % p_train
 print '\nAccuracy on test set: %g' % p_test
-
 print '\nConfusion matrix:\n',cm
-
-# print '\nTrue vs Predicted\n', c_[y_test, y_test_pred]
 
 
 print "Total run time:", time.time() - start_time, "seconds"
