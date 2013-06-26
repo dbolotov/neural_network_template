01a405f7189b210951c925ac92bca56d32e43a4e
dbolotov <dbolotov@live.com>
2013-06-26 10:42:52 -0700

    added ability to pass data filename as arg

--- nn_template.py
+++ nn_template.py
@@ -12,9 +12,16 @@ from nn_helper_functions import *
 
 
 start_time = time.time()
+print "\nScript started at %s \n" % time.strftime("%H:%M:%S",time.localtime(start_time))
 
-# load, shuffle and normlaize data
-X_full,y_full = data_preprocess('datasets/wdbc_data.csv')
+# load, shuffle and normalize data
+try:
+	data_filename = sys.argv[1]
+except:
+	data_filename = 'fisher_iris.csv' #default dataset to use
+
+print "Using %s\n" % data_filename
+X_full,y_full = data_preprocess('datasets/' + data_filename)
 
 # split data into training and test sets
 X,y,X_test,y_test = split_data(X_full, y_full, train_frac = 0.70)
@@ -33,7 +40,7 @@ print '\nAccuracy on test set: %g' % p_test
 print '\nConfusion matrix:\n',cm
 
 
-print "Total run time:", time.time() - start_time, "seconds"
+print "\nTotal run time: %.4f seconds" % (time.time() - start_time)
 
 
 
