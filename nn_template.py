3f8f782cc60142ef5b907f83e55422a90c35fb34
dbolotov <dbolotov@live.com>
2013-07-01 10:32:25 -0700

    using nnhf as module

--- nn_template.py
+++ nn_template.py
@@ -8,8 +8,8 @@
 #Code is based on ml-class.org, Ex.4.
 
 
-from nn_helper_functions import *
-
+#from nn_helper_functions import *
+import nnhf,time,sys
 
 start_time = time.time()
 print "\nScript started at %s \n" % time.strftime("%H:%M:%S",time.localtime(start_time))
@@ -20,20 +20,23 @@ try:
 except:
 	data_filename = 'fisher_iris.csv' #default dataset to use
 
+
 print "Using %s\n" % data_filename
-X_full,y_full = data_preprocess('datasets/' + data_filename)
+X_full,y_full = nnhf.data_preprocess('datasets/' + data_filename)
+
+
 
 # split data into training and test sets
-X,y,X_test,y_test = split_data(X_full, y_full, train_frac = 0.70)
+X,y,X_test,y_test = nnhf.split_data(X_full, y_full, train_frac = 0.70)
 
 # learn network parameters
-Theta1, Theta2 = nn_train(X,y, lam=1, hidden_layer_size = 10)
+Theta1, Theta2 = nnhf.nn_train(X,y, lam=1, hidden_layer_size = 10)
 
 # evaluate performance
-p_train = pred_accuracy(Theta1, Theta2, X, y)
-p_test = pred_accuracy(Theta1, Theta2, X_test, y_test)
-y_test_pred = predict(Theta1, Theta2, X_test)
-cm = confusion_matrix(y_test, y_test_pred)
+p_train = nnhf.pred_accuracy(Theta1, Theta2, X, y)
+p_test = nnhf.pred_accuracy(Theta1, Theta2, X_test, y_test)
+y_test_pred = nnhf.predict(Theta1, Theta2, X_test)
+cm = nnhf.confusion_matrix(y_test, y_test_pred)
 
 print '\nAccuracy on training set: %g' % p_train
 print '\nAccuracy on test set: %g' % p_test
